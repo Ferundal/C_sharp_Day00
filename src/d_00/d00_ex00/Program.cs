13371d136afa4b8e178832f0d3cec373a0c8fa00
ferundal <46624679+Ferundal@users.noreply.github.com>
2022-11-29 12:24:03 +0400

    second started

---
+++ src/d_00/d00_ex00/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+double sum;
+double rate;
+int term;
+
+if (!double.TryParse(args[0], out sum) || !double.TryParse(args[1], out rate) || !int.TryParse(args[2], out term)
+    || sum <= 0 || term <= 0)
+{
+    Console.WriteLine("Something went wrong. Check your input and retry.");
+    return;
+}
+
+var dateTime = DateTime.Now;
+var cultureInfo = new CultureInfo("en-GB");
+var interestRate = rate / 12 / 100;
+var monthlyPayment = sum * interestRate * Math.Pow(1 + interestRate, term) / (Math.Pow(1 + interestRate, term) - 1);
+var totalDebtBalance = sum;
+double principalDebit;
+Thread.CurrentThread.CurrentCulture = cultureInfo;
+for (int currentMonth = 1; currentMonth <= term; currentMonth++)
+{
+    var currentDateTime = dateTime.AddMonths(1);
+    var monthlyPaymentInterest = totalDebtBalance * rate * (currentDateTime - dateTime).Days / (100 * (dateTime.AddYears(1) - dateTime).Days);
+    if (currentMonth != term)
+    {
+        principalDebit = monthlyPayment - monthlyPaymentInterest;
+        totalDebtBalance -= principalDebit;
+    }
+    else
+    {
+        monthlyPayment = totalDebtBalance;
+        principalDebit = monthlyPayment - monthlyPaymentInterest;
+        totalDebtBalance = 0;
+    }
+    Console.WriteLine($"{currentMonth}\t" +
+                      $"{dateTime.ToString("MM/dd/yyyy",cultureInfo)}\t" +
+                      $"{monthlyPayment, -10:N2}\t" +
+                      $"{principalDebit, -10:N2}\t" +
+                      $"{monthlyPaymentInterest, -10:N2}" +
+                      $"{totalDebtBalance, -10:N2}");
+}
---
+++ src/d_00/d00_ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+
+string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"dictionary");
+if (!File.Exists(path))
+{
+    Console.WriteLine("Can't found file");
+    return (-1);
+}
+string[] names = File.ReadAllLines(path);
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
